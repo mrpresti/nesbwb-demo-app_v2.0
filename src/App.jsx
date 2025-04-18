0f5acfa2519e272b61fb8896fac04478ebea97bd
mrpresti <mrpresti@gmail.com>
2025-04-18 14:43:18 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>NES-BWB Demo App</h1>
+      <p>This is a basic starter for your NES-BWB Field Services web app.</p>
+    </div>
+  );
+}
+
+export default App;
